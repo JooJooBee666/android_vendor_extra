87b011d719fc2d26748334cd5df2fbca0468a4ac
JooJooBee666 <desire4homicide@gmail.com>
2023-06-17 14:53:48 -0400

    product.mk: Force MindTheGapps install by removing WITH_GMS ifdef.
    Change-Id: I7c4d881fe119b73d7e63f53eb7fae950b0050916

--- product.mk
+++ product.mk
@@ -3,9 +3,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.sys.adb.shell=/system_ext/bin/bash
 
 # Google Apps
-ifeq ($(WITH_GMS), true)
 $(call inherit-product-if-exists, vendor/gapps/arm64/arm64-vendor.mk)
-endif
 
 # iperf3
 PRODUCT_PACKAGES += iperf3
